9d223adde52acb13cd10d1ebca692d0d2e5ef525
Georges Alkhouri <georges.alkhouri@gmail.com>
2021-01-31 20:17:28 +0100

    Added neovim plugin install

--- install.sh
+++ install.sh
@@ -54,3 +54,7 @@ pyenv deactivate
 
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install Plugins
+nvim +PlugInstall +qall
+
